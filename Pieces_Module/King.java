acd18b5f71f23665a4d7f6943889910c903b64e7
Joshua1014 <josh.hansen.smith@outlook.com>
2024-04-25 17:24:59 +1000

    king_class_movement

---
+++ Pieces_Module/King.java
@@ -0,0 +1,21 @@
+package Pieces_Module;
+
+public class King extends Piece {
+    public King(int x, int y, boolean isWhite){
+        super(x, y, isWhite);
+    }
+
+    @Override
+    public boolean canMove(int destX, int destY, Piece[][] board) {
+        if (!validMove(destX, destY, board)) {
+            return false;
+        }
+
+        //King specific movement validation
+        int xDiff = Math.abs(destX - x);
+        int yDiff = Math.abs(destY - y);
+
+        //Check if the King moves only one square in any direction
+        return xDiff <= 1 && yDiff <= 1;
+    }
+}
